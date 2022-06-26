be2915454d60cf87b80a8b64f5826b724ad12e46
Clinton-M <107317012+Clinton-M@users.noreply.github.com>
2022-06-26 10:32:02 -0500

    solo ejercicio

--- ejercicios/src/ejercicio1.java
+++ ejercicios/src/ejercicio1.java
@@ -1,5 +1,6 @@
 public class ejercicio1 {
     public static void main(String[] args) {
         System.out.println("hola soy un ejercicio en proceso");
+        System.out.println("holaaa");
     }
 }
